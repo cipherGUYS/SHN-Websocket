16b672e69c9d231237d63b2ab31b8c0c1325e32a
sreedhar <sreedharks2003@gmail.com>
2022-10-15 22:46:11 +0530

    from bash

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection',(socket)=>{
+    console.log('user connected');
+    socket.on('disconnect',()=>{
+        console.log("disconnect");
+    });
+    socket.on('user joined',(props)=>{
+            io.emit('user joined',props);
+    });
+    socket.on("chat-message",(msg)=>{
+        io.emit("chat-message",msg);
+    })
+})
+http.listen(3000,()=>{
+    console.log("started server at 3000");
+})
