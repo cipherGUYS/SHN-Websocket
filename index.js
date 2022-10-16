3b6558b959426ac53aaef4fd46a8011c87dda878
Tomin Joy <tominjk007@gmail.com>
2022-10-16 07:07:53 +0530

    final

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const app = require('express')();
+const express_ = require('express');
+const app =express_();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const urlRegex=/(((https?:\/\/)|(www\.))[^\s]+)/g;
+app.use(express_.static(__dirname+'/public'))
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/entry.html');
 });
+
 io.on('connection',(socket)=>{
     console.log('user connected');
     socket.on('disconnect',()=>{
@@ -12,9 +17,39 @@ io.on('connection',(socket)=>{
     socket.on('user joined',(props)=>{
             io.emit('user joined',props);
     });
-    socket.on("chat-message",(msg)=>{
-        io.emit("chat-message",msg);
+    socket.on("chat-message",(msg,isurl)=>{
+        const as_url=msg.match(urlRegex,msg);
+        const url=`<a href="${as_url}" style="color:'blue'">${as_url}</a>`;
+        msg=msg.replace(as_url,url);
+        console.log(msg);
+                io.emit("chat-message",msg);
     })
+    socket.on('create-room',(id)=>{
+       
+        room = 10;
+        socket.to(room).emit("newmessage",'hello from room !')
+        app.get('/room/:id',(req,res)=>{
+            console.log(req.params.id);
+            res.sendFile(__dirname+'/index.html')
+        })
+        
+    })
+    socket.on('join-room',(id)=>{
+       
+        room = id;
+        
+        app.get('/joinroom/:props',(req,res)=>{
+            console.log(req.params.props);
+            res.sendFile(__dirname+'/index.html')
+        })
+        socket.join(id);
+        socket.on("newmessage",()=>{
+            io.to(room).emit("newmessage","hello this from room")
+        })
+        
+    })
+
+
 })
 
 const PORT = process.env.PORT || 3000 
---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+document.getElementById("jRoom").addEventListener("click",()=>{
+    // document.getElementById("cRoom").classList.add("d-none");
+    // document.getElementById("back").classList.remove("d-none");
+    // document.getElementById("codeI").classList.remove("d-none");
+    // document.getElementById("codeI").focus();
+    // console.log("clicked")
+    joinroom();
+})
+document.getElementById("cRoom").addEventListener("click",()=>{
+    // document.getElementById("jRoom").classList.add("d-none");
+    // document.getElementById("ccodeI").classList.remove("d-none");
+    // // document.getElementById("ccodeI").focus();
+    
+    // document.getElementById("back").classList.remove("d-none");
+    // console.log("clicked")
+    
+    createroom();
+})
+document.getElementById("back").addEventListener("click",()=>{
+    document.getElementById("jRoom").classList.remove("d-none");    
+    document.getElementById("cRoom").classList.remove("d-none");
+    document.getElementById("ccodeI").classList.add("d-none");
+    document.getElementById("codeI").classList.add("d-none");
+    document.getElementById("back").classList.add("d-none");
+    console.log("body clicked")
+})
+docume
+console.log("hi");
+    
