8d4c8c4d3256271d63ae3e1f137a37f0750377f8
Tomin Joy <tominjk007@gmail.com>
2022-10-27 22:09:49 +0530

    chore: room added

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express_ = require('express');
 const app =express_();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const {v4:uidv4} = require('uuid');
 const urlRegex=/(((https?:\/\/)|(www\.))[^\s]+)/g;
 app.use(express_.static(__dirname+'/public'))
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/entry.html');
 });
-
+var room_tracker=[];
+var flag=1;
+var room;
 io.on('connection',(socket)=>{
     console.log('user connected');
     socket.on('disconnect',()=>{
@@ -17,36 +20,42 @@ io.on('connection',(socket)=>{
     socket.on('user joined',(props)=>{
             io.emit('user joined',props);
     });
-    socket.on("chat-message",(msg,isurl)=>{
+    socket.on("chat-message",(msg)=>{
         const as_url=msg.match(urlRegex,msg);
         const url=`<a href="${as_url}" style="color:'blue'">${as_url}</a>`;
         msg=msg.replace(as_url,url);
-        console.log(msg);
-                io.emit("chat-message",msg);
+        io.to(room).emit("chat-message",msg);
     })
     socket.on('create-room',(id)=>{
        
-        room = 10;
-        socket.to(room).emit("newmessage",'hello from room !')
-        app.get('/room/:id',(req,res)=>{
-            console.log(req.params.id);
-            res.sendFile(__dirname+'/index.html')
-        })
+        room = uidv4();
+        room_tracker.push(room);
+        socket.join(room)
+        io.to(room).emit("roomid-return",room)
         
     })
     socket.on('join-room',(id)=>{
-       
-        room = id;
-        
-        app.get('/joinroom/:props',(req,res)=>{
-            console.log(req.params.props);
-            res.sendFile(__dirname+'/index.html')
-        })
-        socket.join(id);
-        socket.on("newmessage",()=>{
-            io.to(room).emit("newmessage","hello this from room")
-        })
-        
+        console.log(id);
+        if(room_tracker.length==0){
+            console.log("no rooms");
+            socket.emit("joinroom-error",'no rooms:please create a new room');
+            
+        }
+        for (let i = 0; i < room_tracker.length; i++) {
+            if(room_tracker[i]==id){
+                room=id;
+                flag=0;
+                break;
+            }
+            
+        }
+        if(flag==1){
+            console.log("error no room found ");
+            socket.emit("joinroom-error",'error no such room found !');
+        }else{
+            socket.join(room);
+            io.to(room).emit("roomid-return",room);
+        }    
     })
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 document.getElementById("jRoom").addEventListener("click",()=>{
-    // document.getElementById("cRoom").classList.add("d-none");
-    // document.getElementById("back").classList.remove("d-none");
-    // document.getElementById("codeI").classList.remove("d-none");
-    // document.getElementById("codeI").focus();
-    // console.log("clicked")
-    joinroom();
+    document.getElementById("cRoom").classList.add("d-none");
+    document.getElementById("back").classList.remove("d-none");
+    document.getElementById("uuid4").classList.remove("d-none");
+    document.getElementById("uuid4").focus();
+    console.log("clicked")
+    // joinroom();
 })
 document.getElementById("cRoom").addEventListener("click",()=>{
     // document.getElementById("jRoom").classList.add("d-none");
@@ -20,10 +20,9 @@ document.getElementById("back").addEventListener("click",()=>{
     document.getElementById("jRoom").classList.remove("d-none");    
     document.getElementById("cRoom").classList.remove("d-none");
     document.getElementById("ccodeI").classList.add("d-none");
-    document.getElementById("codeI").classList.add("d-none");
+    document.getElementById("uuid4").classList.add("d-none");
     document.getElementById("back").classList.add("d-none");
     console.log("body clicked")
 })
-docume
-console.log("hi");
+// console.log("hi");
     
